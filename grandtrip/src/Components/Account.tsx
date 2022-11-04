29974376867db2a7cbe2bac247ac6865dd92769d
Timur <timurturovets@mail.ru>
2022-11-05 00:05:38 +0300

    Начало работы над личным кабинетом

--- grandtrip/src/Components/Account.tsx
+++ grandtrip/src/Components/Account.tsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { AuthContextConsumer } from '../AuthContext'
 
 export default class Account extends Component {
     render() {
-        return <div>
-            <h1>Личный кабинет (в разработке)</h1>
-        </div>
+        return <AuthContextConsumer>{({info})=><div>
+            <h1 className="text-center">Личный кабинет</h1>
+            <h5>Вы вошли как {info?.username}</h5>
+            <button className="btn btn-danger">Выйти из аккаунта</button>
+        </div>}</AuthContextConsumer>
+    }
+
+    handleLogOff = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        localStorage.clear();
+        window.location.reload();
     }
 }
